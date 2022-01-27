79b3028d6bf883f315159894de43653f29c0e9e7
DevHelper <88221415+vijaibishnoi@users.noreply.github.com>
2022-01-27 16:38:48 +0530

    Update script.js

--- tools/placeholder-image/script.js
+++ tools/placeholder-image/script.js
@@ -1,8 +1,15 @@
-const name = document.getElementById('toolinputdomain);
+const width = document.getElementById('imgwidth');
+const height = document.getElementById('imgheight');
+const format = document.getElementById('imgformat');
+const size = document.getElementById('imgfontsize');
+const color = document.getElementById('imgcolor');
+const background = document.getElementById('background');
+const text = document.getElementById('imgtext');
+
 
 const generator = document.getElementById('generator');
-const backlinks = document.getElementById('tooloutput');
+const placeholder = document.getElementById('tooloutput');
 
-generator.addEventListener('submit', () => {
-    backlinks.innerHTML = `<div class="aadi"><div class="ubi-9"><p>https://www.similarsites.com/site/${name.value}</p></div><div class="ubi-3"><a class="btn btn-success" href="https://www.similarsites.com/site/${name.value}" target="blank">Check</a></div></div><div class="aadi"><div class="ubi-9"><p>https://www.alexa.com/siteinfo/${name.value}</p></div><div class="ubi-3"><a class="btn btn-success" href="https://www.alexa.com/siteinfo/${name.value}" target="blank">Check</a></div></div><div class="aadi"><div class="ubi-9"><p>https://www.builtwith.com/${name.value}</p></div><div class="ubi-3"><a class="btn btn-success" href="https://www.builtwith.com/${name.value}" target="blank">Check</a></div></div><div class="aadi"><div class="ubi-9"><p>https://www.who.is/whois/${name.value}</p></div><div class="ubi-3"><a class="btn btn-success" href="https://www.who.is/whois/${name.value}" target="blank">Check</a></div></div>`
+generator.addEventListener('click', () => {
+    placeholder.innerHTML = `<img src="https://placehold.jp/${size.value}/${background.value}/${color.value}/${width.value}x${height.value}.${format.value}?text=${text.value}" style="max-width:100%">`
 });
